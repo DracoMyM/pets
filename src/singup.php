a006cdc3df3304a79cf7f2f2decd2d226cd1a06c
Daniel_Moncayo <daniel.f.m.m2.0@gmail.com>
2024-05-03 08:16:36 -0500

    enviar todos los archivos

---
+++ src/singup.php
@@ -0,0 +1,15 @@
+<?php
+
+    $fullname = $_POST['fname'];
+    $email = $_POST['email'];
+    $passwd = $_POST['passwd'];
+    $enc_pass = md5($passwd);
+
+    echo "Your fullname: ". $fullname."<br>";
+    echo "Your Email: ". $email."<br>";
+    echo "Your Password: ". $passwd."<br>" ;
+    echo "Your Password Enc". $enc_pass;
+
+    
+
+?>
