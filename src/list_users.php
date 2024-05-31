1e65e000695515cad2f23a8b5e387f6198b0e31a
Daniel_Moncayo <daniel.f.m.m2.0@gmail.com>
2024-05-31 08:58:43 -0500

    chore: delete landing/readme.txt and src/readme.txt

---
+++ src/list_users.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PETS | LIST USERS</title>
+</head>
+<body>
+    <CENter><H1>list users</H1></CENter>
+    <table border=1 aling=center>
+        <tr>
+            <th>Fullname</th>
+            <th>Email</th>
+            <th>status</th>
+            <th>foto</th>
+            <th>...</th>
+        </tr>
+    </table>
+</body>
+</html>
